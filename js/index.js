07a96dd3a9514ba0d4313250e7dafe110b684730
Rahul jha <jha45507@gmail.com>
2023-03-17 11:08:35 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+function open_hamburger() {
+    document.querySelector(".phone_view_unorderlist").style.display = "block";
+    document.querySelector(".hamburger_bar_icon").style.display = "none";
+    document.querySelector(".close_hamburger_icon").style.display = "block";
+}
+
+function close_hamburger(){
+    document.querySelector(".phone_view_unorderlist").style.display = "none";
+    document.querySelector(".hamburger_bar_icon").style.display = "block";
+    document.querySelector(".close_hamburger_icon").style.display = "none";
+}
+
+document.body.addEventListener('scroll', function() {
+    // console.log(';{};');
+    console.log("this is my")
+} );
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 0){
+        document.querySelector(".desktop_view_navbar").style.background = "#333333"
+    }
+    else{
+        document.querySelector(".desktop_view_navbar").style.background = "transparent"
+    }
+})
