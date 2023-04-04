b42982486d0d3ce95b3cfb0d2773458542d4e060
Rahul jha <jha45507@gmail.com>
2023-04-04 18:14:09 +0530

    commit code

--- js/index.js
+++ js/index.js
@@ -4,22 +4,25 @@ function open_hamburger() {
     document.querySelector(".close_hamburger_icon").style.display = "block";
 }
 
-function close_hamburger(){
+function close_hamburger() {
     document.querySelector(".phone_view_unorderlist").style.display = "none";
     document.querySelector(".hamburger_bar_icon").style.display = "block";
     document.querySelector(".close_hamburger_icon").style.display = "none";
 }
 
-document.body.addEventListener('scroll', function() {
-    // console.log(';{};');
-    console.log("this is my")
-} );
-
 window.addEventListener("scroll", () => {
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         document.querySelector(".desktop_view_navbar").style.background = "rgb(44 73 73)"
     }
-    else{
+    else {
         document.querySelector(".desktop_view_navbar").style.background = "transparent"
     }
 })
+
+$(".phone-view-navigation-item").click(function(){
+    if (document.querySelector(".phone_view_unorderlist").style.display = "block") {
+        document.querySelector(".phone_view_unorderlist").style.display = "none";
+        document.querySelector(".hamburger_bar_icon").style.display = "block";
+        document.querySelector(".close_hamburger_icon").style.display = "none";
+    }
+});
