09649f27b2f23b35c116102759da6f7f43a909e9
Rahul jha <jha45507@gmail.com>
2023-04-05 16:57:53 +0530

    commit code

---
+++ contact.php
@@ -0,0 +1,59 @@
+<form action="" id="contect-form">
+    <div class="contect_form">
+            <div class="contect_form_child">
+                <h1 class="">Online <span class="Appointment">Appointment</span> Request Form</h1>
+                <div class="contect_form_children">
+                    <div class="field_parent_div">
+                        <div>
+                            <label for="">Name</label>
+                        </div>
+                        <div class="field_div">
+                            <input type="text" name="" id="">
+                        </div>
+                    </div>
+                    <div class="field_parent_div">
+                        <div>
+                            <label for="">Number</label>
+                        </div>
+                        <div class="field_div">
+                            <input type="text" name="" id="mobile" value="+91">
+                        </div>
+                    </div>
+                </div>
+                <div class="contect_form_children">
+                    <div class="field_parent_div">
+                        <div>
+                            <label for="">Date</label>
+                        </div>
+                        <div class="field_div">
+                            <input type="date" name="" id="" class="date_type">
+                        </div>
+                    </div>
+                    <div class="field_parent_div">
+                        <div>
+                            <label for="">Treatment</label>
+                        </div>
+                        <div class="field_div">
+                            <select name="" id="" class="">
+                                <option value="">---------</option>
+                                <option value="">Dental exam and teeth cleaning</option>
+                                <option value="">Dental emergency</option>
+                                <option value="">Dental implants</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="textarea_parent_div">
+                    <div class="textarea_div">
+                        <label for="">Description</label>
+                    </div>
+                    <div class="textarea_div">
+                        <textarea class="textarea" name="" id=""></textarea>
+                    </div>
+                </div>
+                <div class="submit_button_div">
+                    <button class="submit_button">Submit</button>
+                </div>
+            </div>
+    </div>
+</form>
